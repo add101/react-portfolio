850a805ccaf6e88f42e5f114b30635fd4ada060b
Adrian Lamour <recreate3d@gmail.com>
2024-03-12 17:34:24 +0000

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,43 @@ import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import ProjectGallery from './ProjectGallery.jsx'
 import Projects from './Projects.jsx'
+// import './index.css'
+import './stylesheet.css'
 
 function App() {
   
   return (
     <>
+
+<header>header</header>
+    <div class="wrapper">
+        <div class="row">
+            <section id="avatar">avatar</section>
+            <section id="intro">intro
+                <p>this section describes the page</p>
+
+            </section>
+        </div>        
+        <section id="gallery">gallery</section>
+        <section id="">
+            <div class="portfolio-row">
+                <div class="portfolio-content">
+                        portfolio1
+                </div>
+                <div class="portfolio-content">
+                    portfolio 2
+                </div>
+                <div class="portfolio-content">
+                    portfolio 2
+                </div>
+                <div class="portfolio-content">
+                    portfolio 2
+                </div>
+            </div>
+        </section>        
+        <section id="tools">tools</section>
+    </div>    
+    <footer>footer</footer>
 
       <Header/>
       <hr></hr>
---
+++ src/Fruit.jsx
@@ -0,0 +1,11 @@
+
+
+function Fruit(){
+    return(
+        <ul>
+            <li></li>
+        </ul>
+    )
+}
+
+export default Fruit
